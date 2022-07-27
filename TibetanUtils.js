d2338fd583aeb715abf7960fc692f8f605810eb2
Andrew Hughes <andrewcarterhughes@gmail.com>
2022-07-27 15:56:03 -0700

    working on regex

---
+++ TibetanUtils.js
@@ -0,0 +1,264 @@
+/*
+ * Good summation: https://www.rigpawiki.org/index.php?title=Tibetan_Grammar_-_Formation_of_the_Tibetan_Syllable
+ */
+
+import jsEwts from './jsewts'
+
+export const unicodeConsonants = [
+  'ཀ', 'ཁ', 'ག', 'ང',
+  'ཅ', 'ཆ', 'ཇ', 'ཉ',
+  'ཏ', 'ཐ', 'ད', 'ན',
+  'པ', 'ཕ', 'བ', 'མ',
+  'ཙ', 'ཚ', 'ཛ', 'ཝ',
+  'ཞ', 'ཟ', 'འ', 'ཡ',
+  'ར', 'ལ', 'ཤ', 'ས',
+  'ཧ', 'ཨ']
+
+/*
+    (?<prefix>  # handle prefixes
+      (g(?=(?:.?)(?:c|ny|t|d|n|ts|zh|z|.y|sh|s)))|
+      (d(?=(?:.?)(?:k|g|ng|p|b|m|ky|gy|py|by|my|kr|gr|pr|br)))|
+      (b(?=(?:.?)(?:k|g|c|t|d|ts|zh|z|sh|s|ky|gy|kr|gr|rl|sl|rk|rg|rng|rj|rny|rt|rd|rn|rts|rdz|lt|ld|st|sk|sg|sng|sny|st|sd|sn|sts|rky|rgy|sky|sgy|skr|sgr)))|
+      (m(?=(?:.?)(?:kh|g|ng|ch|j|ny|th|d|n|tsh|dz|khy|gy|khr|gr)))|
+      ('(?=(?:.?)(?:kh|g|ch|j|th|d|ph|b|tsh|dz|khy|gy|phy|by|khr|gr|dr|phr|br)))
+    )?
+    \.?  # eat the dot forcing separation between prefix and main stack
+    (?<superscript>[rls]?)
+    (?<root>k|kh|g|ng|c|ch|j|ny|t|th|d|n|p|ph|b|m|ts|tsh|dz|w|zh|z|'|y|r|l|sh|s|h)
+    (?<subscript>[yrlw]?)
+    (?<vowel>[aeiou])
+    (  # suffix or 6th/terminator
+        ((?<suffix>g|ng|d|n|b|m|'|r|l|s){0,1}(?<suffix2>g|s)?$)|
+        (?<sixth>'[aeiou])
+    )
+*/
+export const wylieRegex = /(?<prefix>(g(?=(?:.?)(?:c|ny|t|d|n|ts|zh|z|.y|sh|s)))|(d(?=(?:.?)(?:k|g|ng|p|b|m|ky|gy|py|by|my|kr|gr|pr|br)))|(b(?=(?:.?)(?:k|g|c|t|d|ts|zh|z|sh|s|ky|gy|kr|gr|rl|sl|rk|rg|rng|rj|rny|rt|rd|rn|rts|rdz|lt|ld|st|sk|sg|sng|sny|st|sd|sn|sts|rky|rgy|sky|sgy|skr|sgr)))|(m(?=(?:.?)(?:kh|g|ng|ch|j|ny|th|d|n|tsh|dz|khy|gy|khr|gr)))|('(?=(?:.?)(?:kh|g|ch|j|th|d|ph|b|tsh|dz|khy|gy|phy|by|khr|gr|dr|phr|br))))?\.?(?<superscript>[rls]?)(?<root>k|kh|g|ng|c|ch|j|ny|t|th|d|n|p|ph|b|m|ts|tsh|dz|w|zh|z|'|y|r|l|sh|s|h)(?<subscript>[yrlw]?)(?<vowel>[aeiou])(((?<suffix>g|ng|d|n|b|m|'|r|l|s){0,1}(?<suffix2>g|s)?$)|(?<sixth>'[aeiou]))/
+
+export const isWylie = (input) => {
+  return input === jsEwts.toWylie(jsEwts.fromWylie(input))
+}
+
+/*
+export function breakWylie(input) {
+  let stack = input
+  const info = {}
+
+  // remove trailing 'i or 'o (6th case or termination)
+  const postfix = stack.match(/'i|'o$/m)
+  if (postfix) {
+    info.postfix = postfix[0]
+    stack.replace(info.postfix, '')
+  }
+
+  const vowel = stack.match([aeiou])
+  if (!vowel) throw Error ("must include vowel")
+  info.vowel = vowel[0]
+  const split = stack.splt(info.vowel)
+
+  const suffixes = split[1]
+  const prefixAndRootStack = split[0]
+
+  const prefixRegex = /(?<prefix>(g(?=(?:.?)(?:c|ny|t|d|n|ts|zh|z|.y|sh|s)))|(d(?=(?:.?)(?:k|g|ng|p|b|m|ky|gy|py|by|my|kr|gr|pr|br)))|(b(?=(?:.?)(?:k|g|c|t|d|ts|zh|z|sh|s|ky|gy|kr|gr|rl|sl|rk|rg|rng|rj|rny|rt|rd|rn|rts|rdz|lt|ld|st|sk|sg|sng|sny|st|sd|sn|sts|rky|rgy|sky|sgy|skr|sgr)))|(m(?=(?:.?)(?:kh|g|ng|ch|j|ny|th|d|n|tsh|dz|khy|gy|khr|gr)))|('(?=(?:.?)(?:kh|g|ch|j|th|d|ph|b|tsh|dz|khy|gy|phy|by|khr|gr|dr|phr|br))))/
+  const prefixMatch = prefixAndRootStack.match(prefixRegex)
+
+
+}
+*/
+
+export const matchRegex = (string) => {
+  const wylie = toWylieIfNecessary(string)
+  const result = wylie.match(wylieRegex)
+  if (!result) return false
+
+  function normalize(toCheck) {
+    return toCheck ? toCheck : null
+  }
+
+  return {
+    match: normalize(result[0]),
+    prefix: normalize(result.groups.prefix),
+    superscript: normalize(result.groups.superscript),
+    root: normalize(result.groups.root),
+    subscript: normalize(result.groups.subscript),
+    vowel: normalize(result.groups.vowel),
+    suffix: normalize(result.groups.suffix),
+    suffix2: normalize(result.groups.suffix2),
+    sixth: normalize(result.groups.sixth)
+    }
+}
+
+export const row1Unicode = unicodeConsonants.slice(0, 4)
+export const row2Unicode = unicodeConsonants.slice(4, 8)
+export const row3Unicode = unicodeConsonants.slice(8, 12)
+export const row4Unicode = unicodeConsonants.slice(12, 16)
+export const row5Unicode = unicodeConsonants.slice(16, 20)
+export const row6Unicode = unicodeConsonants.slice(20, 24)
+export const row7Unicode = unicodeConsonants.slice(24, 28)
+export const row8Unicode = unicodeConsonants.slice(28, 30)
+export const finall11Unicode = unicodeConsonants.slice(19, 30)
+
+export const wylieConsonants = [
+  'ka', 'kha', 'ga', 'nga',
+  'ca', 'cha', 'ja', 'nya',
+  'ta', 'tha', 'da', 'na',
+  'pa', 'pha', 'ba', 'ma',
+  'tsa', 'tsha', 'dza', 'wa',
+  'zha', 'za', '\'a', 'ya',
+  'ra', 'la', 'sha', 'sa',
+  'ha', 'a']
+
+export const row1Wylie = wylieConsonants.slice(0, 4)
+export const row2Wylie = wylieConsonants.slice(4, 8)
+export const row3Wylie = wylieConsonants.slice(8, 12)
+export const row4Wylie = wylieConsonants.slice(12, 16)
+export const row5Wylie = wylieConsonants.slice(16, 20)
+export const row6Wylie = wylieConsonants.slice(20, 24)
+export const row7Wylie = wylieConsonants.slice(24, 28)
+export const row8Wylie = wylieConsonants.slice(28, 30)
+export const finall11Wylie = wylieConsonants.slice(19, 30)
+
+export const suffixesUnicode = ['ག', 'ང', 'ད', 'ན', 'བ', 'མ', 'འ', 'ར', 'ལ', 'ས']
+export const prefixesUnicode = ['བ', 'ག', 'ད', 'འ', 'མ']
+
+export function toUnicodeIfNecessary(toTest) {
+  return /[a-z]/.test(toTest) ? jsEwts.toUnicode(toTest) : toTest
+}
+
+export function toWylieIfNecessary(toTest) {
+  return /[a-z]/.test(toTest) ? toTest : jsEwts.toWylie(toTest)
+}
+
+function getSupSubRegex(stacks) {
+  return new RegExp("^[" + stacks.replace(/་/g,'') + "]+$")
+}
+
+const canSuperscriptRaStacks = "ཀ་ག་ང་ཇ་ཉ་ཏ་ད་ན་བ་མ་ཙ་ཛ་ཀྱ་གྱ་མྱ་"
+const canSuperscriptRaRegex = getSupSubRegex(canSuperscriptRaStacks)
+export function canSuperscriptRa(letter) {
+  return canSuperscriptRaRegex.test(toUnicodeIfNecessary(letter))
+}
+
+const canSuperscriptLaStacks = "ཀ་ག་ང་ཅ་ཇ་ཏ་ད་པ་བ་ཧ་"
+const canSuperscriptLaRegex = getSupSubRegex(canSuperscriptLaStacks)
+export function canSuperscriptLa(letter) {
+  return canSuperscriptLaRegex.test(toUnicodeIfNecessary(letter))
+}
+
+const canSuperscriptSaStacks = "ཀ་ག་ང་ཉ་ཏ་ད་ན་པ་བ་མ་ཙ་ཀྱ་གྱ་པྱ་བྱ་མྱ་ཀྲ་གྲ་པྲ་མྲ་ནྲ་"
+const canSuperscriptSaRegex = getSupSubRegex(canSuperscriptSaStacks)
+export function canSuperscriptSa(letter) {
+  return canSuperscriptSaRegex.test(toUnicodeIfNecessary(letter))
+}
+
+const canSubscriptYaStacks = "ཀ་ཁ་ག་པ་ཕ་བ་མ་རྐ་རྒ་རྨ་སྐ་སྒ་སྤ་སྦ་སྨ་"
+const canSubscriptYaRegex = getSupSubRegex(canSubscriptYaStacks)
+export function canSubscriptYa(letter) {
+  return canSubscriptYaRegex.test(toUnicodeIfNecessary(letter))
+}
+
+const canSubscriptRaStacks = "ཀ་ཁ་ག་ཏ་ཐ་ད་པ་ཕ་བ་མ་ཤ་ས་ཧ་སྐ་སྒ་སྤ་སྦྲ་སྨ་སྣ་"
+const canSubscriptRaRegex = getSupSubRegex(canSubscriptRaStacks)
+export function canSubscriptRa(letter) {
+  return canSubscriptRaRegex.test(toUnicodeIfNecessary(letter))
+}
+
+const canSubscriptLaStacks = "ཀ་ག་བ་ཟ་ར་ས་"
+const canSubscriptLaRegex = getSupSubRegex(canSubscriptLaStacks)
+export function canSubscriptLa(letter) {
+  return canSubscriptLaRegex.test(toUnicodeIfNecessary(letter))
+}
+
+const canSubscriptWaStacks = "ཀ་ཁ་ག་ཅ་ཉ་ཏ་ད་ཙ་ཚ་ཞ་ཟ་ར་ལ་ཤ་ས་ཧ་གྲ་དྲ་ཕྱ་རྒ་རྩ་"
+const canSubscriptWaRegex = getSupSubRegex(canSubscriptWaStacks)
+export function canSubscriptWa(letter) {
+  return canSubscriptWaRegex.test(toUnicodeIfNecessary(letter))
+}
+
+const stacksWithSuperscribedRa = "རྐ་རྒ་རྔ་རྗ་རྙ་རྟ་རྡ་རྣ་རྦ་རྨ་རྩ་རྫ་རྐྱ་རྒྱ་རྨྱ་"
+
+export function hasSuperscribedRa(toTest) {
+  return /^(g|d|b|m|')?r[^aiuoe]/.test(toWylieIfNecessary(toTest))
+}
+
+export function hasSuperscribedLa(toTest) {
+  return /^(g|th|b|m|')?l[^aiuoe]/.test(toWylieIfNecessary(toTest))
+}
+
+export function hasSuperscribedSa(toTest) {
+  return /^(g|d|b|m|')?s[^aiuoe]/.test(toWylieIfNecessary(toTest))
+}
+
+export function hasSubscribedRa(toTest) {
+  return /^(g|d|b|m|')?[^aiuoe]{1,2}r[aeiou]/.test(toWylieIfNecessary(toTest))
+}
+
+export function hasSubscribedYa(toTest) {
+  return /^(g|d|b|m|')?[^aiuoen]{1,2}y[aeiou]/.test(toWylieIfNecessary(toTest))
+}
+
+export function hasSubscribedLa(toTest) {
+  return /^(g|d|b|m|')?[^aiuoe]{1,2}l[aeiou]/.test(toWylieIfNecessary(toTest))
+}
+
+export function hasSubscript(toTest) {
+  return /^(g|d|b|m|')?[^aiuoen]{1,2}[ryl][aeiou]/.test(toWylieIfNecessary(toTest))
+}
+
+export function hasSuperscript(toTest) {
+  if (hasSubscribedLa(toTest)) return false
+  else return /^(g|d|b|m|')?[lrs][^aiuoe]{1,2}/.test(toWylieIfNecessary(toTest))
+}
+
+export function hasSubscriptOrSuperscript(toTest) {
+  return hasSubscript(toTest) || hasSuperscript(toTest)
+}
+
+export const rootsForPrefixBaUnicode = 'ཀ ཅ ཏ ཙ ག ང ཇ ཉ ད ན ཛ ཞ ཟ ར ཤ ས'.split(' ')
+export const rootsForPrefixAhUnicode = 'ག ཇ ད བ ཛ ཁ ཆ ཐ ཕ ཚ'.split(' ')
+export const rootsForPrefixGaUnicode = 'ཅ ཏ ཙ ཉ ད ན ཞ ཟ ཡ ཤ ས'.split(' ')
+export const rootsForPrefixDaUnicode = 'ཀ པ ག ང བ མ'.split(' ')
+export const rootsForPrefixMaUnicode = 'ཁ ཆ ཐ ཚ ག ཇ ད ཛ ང ཉ ན'.split(' ')
+
+export const rootsForPrefixBaWylie = 'ka ca ta tsa ga nga ja nya da na dza zha za ra sha sa'.split(' ')
+export const rootsForPrefixAhWylie = 'ga ja da ba dza kha cha tha pha tsha'.split(' ')
+export const rootsForPrefixGaWylie = 'ca ta tsa nya da na zha za ya sha sa'.split(' ')
+export const rootsForPrefixDaWylie = 'ka pa ga nga ba ma'.split(' ')
+export const rootsForPrefixMaWylie = 'kha cha tha tsha ga ja da dza nga nya na'.split(' ')
+
+export function superscript(p, root) {
+  return jsEwts.toUnicode(p + jsEwts.toWylie(root))
+}
+
+export function subscript(s, root) {
+  const rootWylie = jsEwts.toWylie(root)
+  return jsEwts.toUnicode(rootWylie.replace('a', s + 'a'))
+}
+
+const canPrefixGa = /^(ཅ|ཉ|ཏ|ད|ན|ཙ|ཞ|ཟ|ཡ|ཤ|ས)/
+const canPrefixGaWylie = /^(c|ny|t|d|n|ts|zh|z|y|sh|s)/
+const canPrefixDa = /^(ཀ|ག|ང|པ|བ|མ|ཀྱ|གྱ|པྱ|བྱ|མྱ|ཀྲ|གྲ|པྲ|བྲ)/
+const canPrefixDaWylie = /^(k|g|ng|p|b|m|ky|gy|py|by|my|kr|gr|pr|br)/
+const canPrefixBa = /^(ཀ|ག|ཅ|ཏ|ད|ཙ|ཞ|ཟ|ཤ|ས|ཀྱ|གྱ|ཀྲ|གྲ|རླ|སླ|རྐ|རྒ|རྔ|རྗ|རྙ|རྟ|རྡ|རྣ|རྩ|རྫ|ལྟ|ལྡ|སྟ|སྐ|སྒ|སྔ|སྙ|སྟ|སྡ|སྣ|སྩ|རྐྱ|རྒྱ|སྐྱ|སྒྱ|སྐྲ|སྒྲ)/
+const canPrefixBaWylie = /^(k|g|c|t|d|ts|zh|z|sh|s|ky|gy|kr|gr|rl|sl|rk|rg|rng|rj|rny|rt|rd|rn|rts|rdz|lt|ld|st|sk|sg|sng|sny|st|sd|sn|sts|rky|rgy|sky|sgy|skr|sgr)/
+const canPrefixMa = /^(ཁ|ག|ང|ཆ|ཇ|ཉ|ཐ|ད|ན|ཚ|ཛ|ཁྱ|གྱ|ཁྲ|གྲ)/
+const canPrefixMaWylie = /^(kh|g|ng|ch|j|ny|th|d|n|tsh|dz|khy|gy|khr|gr)/
+const canPrefixAh = /^(ཁ|ག|ཆ|ཇ|ཐ|ད|ཕ|བ|ཚ|ཛ|ཁྱ|གྱ|ཕྱ|བྱ|ཁྲ|གྲ|དྲ|ཕྲ|བྲ)/
+const canPrefixAhWylie = /^(kh|g|ch|j|th|d|ph|b|tsh|dz|khy|gy|phy|by|khr|gr|dr|phr|br)/
+
+export const canAttachPrefix = (prefix, string) => {
+  const unicode = toUnicodeIfNecessary(string)
+  if (prefix === 'བ') {
+    return canPrefixBa.test(unicode)
+  } else if (prefix === 'འ') {
+    return canPrefixAh.test(unicode)
+  } else if (prefix === 'ག') {
+    return canPrefixGa.test(unicode)
+  } else if (prefix === 'ད') {
+    return canPrefixDa.test(unicode)
+  } else if (prefix === 'མ') {
+    return canPrefixMa.test(unicode)
+  } else {
+    console.log("Should not happen")
+    return false;
+  }
+}
---
+++ utils.test.js
@@ -0,0 +1,260 @@
+import * as tu from './TibetanUtils.js'
+import jsEwts from './jsewts'
+
+test("check rows unicode", () => {
+	expect(tu.row1Unicode).toStrictEqual(['ཀ', 'ཁ', 'ག', 'ང'])
+	expect(tu.row2Unicode).toStrictEqual(['ཅ', 'ཆ', 'ཇ', 'ཉ'])
+	expect(tu.row3Unicode).toStrictEqual(['ཏ', 'ཐ', 'ད', 'ན'])
+	expect(tu.row4Unicode).toStrictEqual(['པ', 'ཕ', 'བ', 'མ'])
+	expect(tu.row5Unicode).toStrictEqual(['ཙ', 'ཚ', 'ཛ', 'ཝ'])
+	expect(tu.row6Unicode).toStrictEqual(['ཞ', 'ཟ', 'འ', 'ཡ'])
+	expect(tu.row7Unicode).toStrictEqual(['ར', 'ལ', 'ཤ', 'ས'])
+	expect(tu.row8Unicode).toStrictEqual(['ཧ', 'ཨ'])
+	expect(tu.finall11Unicode).toStrictEqual([
+		'ཝ',
+		'ཞ', 'ཟ', 'འ', 'ཡ',
+		'ར', 'ལ', 'ཤ', 'ས',
+		'ཧ', 'ཨ'])
+})
+
+
+test("check rows wylie", () => {
+	expect(tu.row1Wylie).toStrictEqual(['ka', 'kha', 'ga', 'nga'])
+	expect(tu.row2Wylie).toStrictEqual(['ca', 'cha', 'ja', 'nya'])
+	expect(tu.row3Wylie).toStrictEqual(['ta', 'tha', 'da', 'na'])
+	expect(tu.row4Wylie).toStrictEqual(['pa', 'pha', 'ba', 'ma'])
+	expect(tu.row5Wylie).toStrictEqual(['tsa', 'tsha', 'dza', 'wa'])
+	expect(tu.row6Wylie).toStrictEqual(['zha', 'za', '\'a', 'ya'])
+	expect(tu.row7Wylie).toStrictEqual(['ra', 'la', 'sha', 'sa'])
+	expect(tu.row8Wylie).toStrictEqual(['ha', 'a'])
+	expect(tu.finall11Wylie).toStrictEqual([
+		'wa',
+		'zha', 'za', '\'a', 'ya',
+		'ra', 'la', 'sha', 'sa',
+		'ha', 'a'])
+})
+
+test("toUnicodeIfNecessary", () => {
+	expect(tu.toUnicodeIfNecessary("ca")).toEqual('ཅ')
+	expect(tu.toUnicodeIfNecessary("ཨ")).toEqual('ཨ')
+})
+
+test("toWylieIfNecessary", () => {
+	expect(tu.toWylieIfNecessary("ca")).toEqual('ca')
+	expect(tu.toWylieIfNecessary("ཨ")).toEqual('a')
+})
+
+test("canSuperscriptRa", () => {
+	expect(tu.canSuperscriptRa("ca")).toEqual(false)
+	expect(tu.canSuperscriptRa("ཅ")).toEqual(false)
+	expect(tu.canSuperscriptRa("tsa")).toEqual(true)
+	expect(tu.canSuperscriptRa("ཏ")).toEqual(true)
+	expect(tu.canSuperscriptRa("gya")).toEqual(true)
+})
+
+test("canSuperscriptLa", () => {
+	expect(tu.canSuperscriptLa("dza")).toEqual(false)
+	expect(tu.canSuperscriptLa("ཛ")).toEqual(false)
+	expect(tu.canSuperscriptLa("ka")).toEqual(true)
+	expect(tu.canSuperscriptLa("ང")).toEqual(true)
+})
+
+
+test("hasSuperscribedRa", () => {
+	expect(tu.hasSuperscribedRa("dza")).toEqual(false)
+	expect(tu.hasSuperscribedRa("ཛ")).toEqual(false)
+	expect(tu.hasSuperscribedRa("grob")).toEqual(false)
+	expect(tu.hasSuperscribedRa("རྫ")).toEqual(true)
+	expect(tu.hasSuperscribedRa("རྒྱེད་")).toEqual(true)
+	expect(tu.hasSuperscribedRa("rkyob")).toEqual(true)
+	expect(tu.hasSuperscribedRa("རྐ")).toEqual(true)
+})
+
+test.concurrent.each("རྐ རྒ རྔ རྗ རྙ རྟ རྡ རྣ རྦ རྨ རྩ རྫ རྐྱ རྒྱ རྨྱ".split(' '))("hasSuperscribedRa true", (input) => {
+	expect(tu.hasSuperscribedRa(input)).toBe(true)
+})
+
+test("hasSuperscribedLa", () => {
+	expect(tu.hasSuperscribedLa("dza")).toEqual(false)
+	expect(tu.hasSuperscribedLa("ཛ")).toEqual(false)
+	expect(tu.hasSuperscribedLa("grob")).toEqual(false)
+	expect(tu.hasSuperscribedLa("ལྡ་")).toEqual(true)
+	expect(tu.hasSuperscribedLa("ལྒེ་")).toEqual(true)
+	expect(tu.hasSuperscribedLa("rkyob")).toEqual(false)
+	expect(tu.hasSuperscribedLa("ཀླིས")).toEqual(false)
+})
+
+test("hasSuperscribedSa", () => {
+	expect(tu.hasSuperscribedSa("dza")).toEqual(false)
+	expect(tu.hasSuperscribedSa("ཛ")).toEqual(false)
+	expect(tu.hasSuperscribedSa("grob")).toEqual(false)
+	expect(tu.hasSuperscribedSa("རྫ")).toEqual(false)
+	expect(tu.hasSuperscribedSa("རྒྱེད་")).toEqual(false)
+	expect(tu.hasSuperscribedSa("rkyob")).toEqual(false)
+	expect(tu.hasSuperscribedSa("skyed")).toEqual(true)
+	expect(tu.hasSuperscribedSa("སྟ་")).toEqual(true)
+	expect(tu.hasSuperscribedSa("smrogs")).toEqual(true)
+})
+
+test("hasSubscribedRa", () => {
+	expect(tu.hasSubscribedRa("rdza")).toEqual(false)
+	expect(tu.hasSubscribedRa("ཛ")).toEqual(false)
+	expect(tu.hasSubscribedRa("rgyob")).toEqual(false)
+	expect(tu.hasSubscribedRa("ཁྲ")).toEqual(true)
+	expect(tu.hasSubscribedRa("khra")).toEqual(true)
+	expect(tu.hasSubscribedRa("khred")).toEqual(true)
+	expect(tu.hasSubscribedRa("ཧྲིག")).toEqual(true)
+	expect(tu.hasSubscribedRa("hrig")).toEqual(true)
+})
+
+test.each("ཀྲ ཁྲ གྲ ཏྲ ཐྲ དྲ པྲ ཕྲ བྲ མྲ ཤྲ སྲ ཧྲ སྐྲ སྒྲ སྤྲ སྦྲ སྨྲ སྣྲ".split(' '))("hasSubscribedRa true", (input) => {
+	expect(tu.hasSubscribedRa(input)).toBe(true)
+})
+
+test("hasSubscribedYa", () => {
+	expect(tu.hasSubscribedYa("rdza")).toEqual(false)
+	expect(tu.hasSubscribedYa("nya")).toEqual(false)
+	expect(tu.hasSubscribedYa("ཉ")).toEqual(false)
+	expect(tu.hasSubscribedYa("ཛ")).toEqual(false)
+	expect(tu.hasSubscribedYa("ཁྲ")).toEqual(false)
+	expect(tu.hasSubscribedYa("khra")).toEqual(false)
+	expect(tu.hasSubscribedYa("ཧྲིག")).toEqual(false)
+	expect(tu.hasSubscribedYa("hrig")).toEqual(false)
+	expect(tu.hasSubscribedYa("gyag")).toEqual(true)
+	expect(tu.hasSubscribedYa("kya")).toEqual(true)
+	expect(tu.hasSubscribedYa("kyid")).toEqual(true)
+	expect(tu.hasSubscribedYa("པྱ")).toEqual(true)
+	expect(tu.hasSubscribedYa("སྒྱ")).toEqual(true)
+	expect(tu.hasSubscribedYa("སྒྱུར")).toEqual(true)
+})
+
+test("hasSubscribedLa", () => {
+	expect(tu.hasSubscribedLa("rdza")).toEqual(false)
+	expect(tu.hasSubscribedLa("ཛ")).toEqual(false)
+	expect(tu.hasSubscribedLa("rgyob")).toEqual(false)
+	expect(tu.hasSubscribedLa("སྒྱ")).toEqual(false)
+	expect(tu.hasSubscribedLa("སྒྱུར")).toEqual(false)
+	expect(tu.hasSubscribedLa("ཧྲིག")).toEqual(false)
+})
+
+test.each("ཀླ གླ བླ ཟླ རླ སླ".split(' '))("hasSubscribedLa true", (input) => {
+	expect(tu.hasSubscribedLa(input)).toBe(true)
+})
+
+test("hasSubscript", () => {
+	expect(tu.hasSubscript("rdza")).toEqual(false)
+	expect(tu.hasSubscript("la")).toEqual(false)
+	expect(tu.hasSubscript("tha")).toEqual(false)
+	expect(tu.hasSubscript("cha")).toEqual(false)
+	expect(tu.hasSubscript("na")).toEqual(false)
+	expect(tu.hasSubscript("nya")).toEqual(false)
+	expect(tu.hasSubscript("sked")).toEqual(false)
+
+	expect(tu.hasSubscript("སླ")).toEqual(true)
+	expect(tu.hasSubscript("gros")).toEqual(true)
+	expect(tu.hasSubscript("thrad")).toEqual(true)
+	expect(tu.hasSubscript("skyed")).toEqual(true)
+})
+
+test("hasSuperscript", () => {
+	expect(tu.hasSuperscript("la")).toEqual(false)
+	expect(tu.hasSuperscript("tha")).toEqual(false)
+	expect(tu.hasSuperscript("cha")).toEqual(false)
+	expect(tu.hasSuperscript("na")).toEqual(false)
+	expect(tu.hasSuperscript("nya")).toEqual(false)
+	expect(tu.hasSuperscript("sla")).toEqual(false)
+	expect(tu.hasSuperscript("སླ")).toEqual(false)
+	expect(tu.hasSuperscript("gros")).toEqual(false)
+	expect(tu.hasSuperscript("bthrad")).toEqual(false)
+
+	expect(tu.hasSuperscript("skyed")).toEqual(true)
+	expect(tu.hasSuperscript("rdza")).toEqual(true)
+	expect(tu.hasSuperscript("dsked")).toEqual(true)
+})
+
+test("wylieRegex", () => {
+
+	console.log("brgyad".match(tu.wylieRegex))
+	expect("brgyad".match(tu.wylieRegex).groups).toEqual({
+        prefix: 'b',
+        superscript: 'r',
+        root: 'g',
+        subscript: 'y',
+        vowel: 'a',
+        suffix: 'd',
+        suffix2: undefined,
+        sixth: undefined
+      })
+	  expect(tu.matchRegex(jsEwts.toUnicode("brgyad"))).toEqual({
+		match: "brgyad",
+        prefix: 'b',
+        superscript: 'r',
+        root: 'g',
+        subscript: 'y',
+        vowel: 'a',
+        suffix: 'd',
+        suffix2: null,
+        sixth: null
+      })
+
+	  expect(tu.matchRegex(jsEwts.toUnicode("dogs"))).toEqual({
+		match: "dogs",
+        prefix: null,
+        superscript: null,
+        root: 'd',
+        subscript: null,
+        vowel: 'o',
+        suffix: 'g',
+        suffix2: 's',
+        sixth: null
+      })
+
+	  expect(tu.matchRegex(jsEwts.toUnicode("dbyings"))).toEqual({
+		match: "dbyings",
+        prefix: 'd',
+        superscript: null,
+        root: 'b',
+        subscript: 'y',
+        vowel: 'i',
+        suffix: 'ng',
+        suffix2: 's',
+        sixth: null
+      })
+
+	  expect(tu.matchRegex("མདོའི")).toEqual({
+		match: "mdo'i",
+        prefix: 'm',
+        superscript: null,
+        root: 'd',
+        subscript: null,
+        vowel: 'o',
+        suffix: null,
+        suffix2: null,
+        sixth: "'i"
+      })
+
+	  expect(tu.matchRegex("g.yag")).toEqual({
+		match: "g.yag",
+        prefix: 'g',
+        superscript: null,
+        root: 'y',
+        subscript: null,
+        vowel: 'a',
+        suffix: 'g',
+        suffix2: null,
+        sixth: null
+      })
+
+	  expect(tu.matchRegex("gyag")).toEqual({
+		match: "gyag",
+        prefix: null,
+        superscript: null,
+        root: 'g',
+        subscript: 'y',
+        vowel: 'a',
+        suffix: 'g',
+        suffix2: null,
+        sixth: null
+      })
+
+})
